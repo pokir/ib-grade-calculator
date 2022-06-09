9ddb003a2e458e4dc57be47c2c0eeb527daeb8f8
pokir <pokir@users.noreply.github.com>
2022-06-09 10:16:36 +0300

    Add assignment component

--- src/components/assignment.jsx
+++ src/components/assignment.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Assignment = () => {
+  const [ weight, setWeight ] = useState(1);
+  const [ numberOfMarks, setNumberOfMarks ] = useState(1);
+  const [ marksAwarded, setMarksAwarded ] = useState(0);
+
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div
+      style={{
+        backgroundColor: 'beige',
+      }}
+    >
+      <h1 contentEditable="true">Paper 1</h1>
+
+    // TODO: Make a number input component with min, max, default, and label
+
+      <p>Weight (out of 100)</p>
+      <input type='number' min='1' max='100'
+        onChange={
+          el => {
+            setWeight(Number.parseInt(el.target.value));
+          }
+        }
+      ></input>
+
+      <p>Number of marks</p>
+      <input type='number' min='1' max='999'
+        onChange={
+          el => {
+            setNumberOfMarks(Number.parseInt(el.target.value));
+          }
+        }
+      ></input>
+
+      <p>Marks awarded (out of {numberOfMarks})</p>
+      <input type='number' min='0' max={numberOfMarks}
+        onChange={
+          el => {
+            setMarksAwarded(Number.parseInt(el.target.value));
+          }
+        }
+      ></input>
+
+      <p></p>
+
     </div>
   );
 };
